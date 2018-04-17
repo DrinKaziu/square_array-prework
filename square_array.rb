6278c76376858b2e8c28163cd630c0e10c50340c
Drin K <drinkaziu@gmail.com>
2018-04-17 19:22:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
 def square_array(array)
+<<<<<<< HEAD
   square_array = []
   array.each do |num|
     square_array << num ** 2
@@ -6,3 +7,9 @@ def square_array(array)
   
   square_array
 end 
+=======
+  array.each do |num|
+    squared_nums = num ** 2
+  end
+end
+>>>>>>> c4d7f6e8270b5dadcd1d032b172710fd23f6f89a
